f263f9721f69099087a9386395a6f1064fef8079
tommy.hutomo <tommy.hutomo30@gmail.com>
2026-02-28 21:26:07 +0800

    adding things

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,10 +2,35 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error', 'warn'], // add 'query' while debugging
-  });
+function createPrismaClient(): PrismaClient {
+  const options: Record<string, unknown> = { log: ['error', 'warn'] };
+  if (process.env.PRISMA_ACCELERATE_URL) options.accelerateUrl = process.env.PRISMA_ACCELERATE_URL;
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+  const client = new PrismaClient(options as any);
+  if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = client;
+  return client;
+}
+
+let _prisma: PrismaClient | undefined = globalForPrisma.prisma;
+
+const handler: ProxyHandler<any> = {
+  get(_target, prop) {
+    if (!_prisma) {
+      _prisma = createPrismaClient();
+    }
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const value = (_prisma as any)[prop];
+    if (typeof value === 'function') return value.bind(_prisma);
+    return value;
+  },
+  set(_target, prop, value) {
+    if (!_prisma) _prisma = createPrismaClient();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    (_prisma as any)[prop] = value;
+    return true;
+  },
+};
+
+export const prisma = new Proxy({}, handler) as unknown as PrismaClient;
